1ee3af90084e470c9da6b3b3cb13c5708ebf9fe2
sofiacostamagna <sofiacostamagna45@gmail.com>
2023-11-23 18:38:02 -0300

    changes img

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -5,8 +5,11 @@ import Image from "next/image";
 export function Introduction() {
   return (
     <div className="bg-[#131424]/60 w-full z-10">
+      {/* A semi-transparent background layer */}
       <div className="w-full h-full bg-gradient-to-r from-[#131424]/10 via-black/30 to-black/10">
+        {/* A container with a grid layout for the introduction section */}
         <div className="container grid items-center h-full px-5 mx-auto md:grid-cols-2 md:text-left">
+          {/* A motion-controlled div that displays an image of Sofia Costamagna */}
           <motion.div
             className="hidden w-auto h-auto mx-auto md:block"
             variants={fadeIn("right", 0.5)}
@@ -15,7 +18,7 @@ export function Introduction() {
             exit="hidden"
           >
             <Image
-              src="/assets/sofi1.JPG"
+              src="/assets/image_6487327.JPG"
               priority
               width="900"
               height="700"
@@ -24,6 +27,7 @@ export function Introduction() {
             />
           </motion.div>
 
+          {/* A motion-controlled div that contains the introduction text and buttons */}
           <motion.div
             variants={fadeIn("left", 0.5)}
             initial="hidden"
@@ -38,14 +42,13 @@ export function Introduction() {
                 Frontend Developer & UX/UI Designer
               </span>
             </h1>
-            <p className="max-w-sm  mb-10 text-lg md:max-w-lg md:mx-0 md:mb-16 md:text-lg">
-              I am a passionate Frontend Developer and UX/UI Designer who is
-              immersed in the exciting world of technology. My focus is on
-              continuous learning and improvement, which drives me to embrace
-              challenges and seek creative solutions. My goal is to bring your
-              ideas to life and deliver exceptional experiences to users. <br />
-              I am excited about the opportunity to create something amazing
-              together!
+            <p className="max-w-sm mb-10 text-lg md:max-w-lg md-0 md-16 md:text">
+              I am a passionate Frontend Developer and UUI who is ed in the
+              exciting world of technology. My focus is on continuous learning
+              and improvement, which drives me to embrace challenges and seek
+              creative solutions. My goal is to bring your ideas to life and
+              deliver exceptional experiences to users. <br />I am excited about
+              the opportunity to create something amazing together!
             </p>
 
             <div className="flex flex-col items-center gap-3 md:gap-10 md:flex-row">
